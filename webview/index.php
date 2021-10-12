14cc70748a267a0b4561189f6730bb9ebc570dbc
schuler-henry <72646334+schuler-henry@users.noreply.github.com>
2021-10-12 16:02:26 +0200

    Initial push

---
+++ webview/index.php
@@ -0,0 +1,42 @@
+<!-- <!DOCTYPE html> -->
+<html lang="de">
+	<head>
+		<meta charset="utf-8">
+		<title>Speedtest WG 7/8 OG Kluftern</title>
+	</head>
+	<body>
+		<meta http-equiv="refresh" content="60" />
+		<h1 style="text-align: center">Speedtest WG 7/8 OK Kluftern</h1>
+		<?php
+			$servername = "localhost";
+			$username = "internet";
+			$password = "internet";
+			$dbname = "InternetDB";
+
+			// Create connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+
+			// Check connection
+			if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			}
+			// echo("Connected successfully" . "<br>");
+
+			$result = $conn->query("SELECT Date, Download, Upload, HostPing FROM Speedtest WHERE NetworkLocation LIKE 'Zerlaut-Kluftern'");
+			
+			if ($result->num_rows > 0) {
+				echo("<table id='InternetDB' hidden='true'>");
+				while ($row = $result->fetch_assoc()) {
+					echo("<tr><th>" . $row["Date"] . ";</th><th>" . $row["Download"] . ";</th><th>" . $row["Upload"] . ";</th><th>" . $row["HostPing"] . ";</th></tr>");
+				}
+				echo("</table>");
+			}
+
+			$conn->close();
+		?>
+		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+		<script src="./js/mainGraph.js"></script>
+		<div id="area_chart_div" style="width: 100%; height: 600px;"></div>
+		<div id="line_chart_div" style="width: 100%; height: 600px;"></div>
+	</body>
+</html>
---
+++ webview/js/mainGraph.js
@@ -0,0 +1,48 @@
+var database = document.getElementById("InternetDB");
+var valueList = database.textContent.split(';');
+
+var valueArray = [['Date', 'Download', 'Upload']];
+for (var i = 0; i < valueList.length/4; i++) {
+    valueArray.push([new Date(valueList[i*4]), parseFloat(valueList[i*4+1]), parseFloat(valueList[i*4+2])]);
+}
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    var data = google.visualization.arrayToDataTable(valueArray);
+
+    var areaOptions = {
+        title: '',
+        hAxis: {title: 'Date',  titleTextStyle: {color: '#333'}},
+        vAxis: {minValue: 0}
+    };
+
+    var lineOptions = {
+        title: '',
+        hAxis: {title: 'Seconds since start',  titleTextStyle: {color: '#333'}},
+        vAxis: {minValue: 0},
+        trendlines: {
+            0: {
+                type: 'polynomial',
+                // type: 'linear',
+                // degree: 5,
+                lineWidth: 2,
+                color: 'blue',
+            },
+            1: {
+                type: 'polynomial',
+                // type: 'linear',
+                //   degree: 9,
+                lineWidth: 2,
+                color: 'red',
+            }
+        }
+    };
+
+    var areaChart = new google.visualization.AreaChart(document.getElementById('area_chart_div'));
+    areaChart.draw(data, areaOptions);
+
+    var lineChart = new google.visualization.ScatterChart(document.getElementById('line_chart_div'));
+    lineChart.draw(data, lineOptions);
+}
